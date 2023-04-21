419dd69fe6e01b2c601312ed385e39bba3be371c
leiw <i.wanglei@foxmail.com>
2023-04-21 15:46:42 +0800

    make await

--- api/VercelBot.py
+++ api/VercelBot.py
@@ -10,11 +10,11 @@ bot = telegram.Bot(token=BOT_TOKEN)
 
 
 @app.route('/webhook', methods=['POST'])
-def webhook():
+async def webhook():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     chat_id = update.message.chat.id
     text = update.message.text
-    bot.sendMessage(chat_id=chat_id, text=text)
+    await bot.sendMessage(chat_id=chat_id, text=text)
     return 'ok'
 
 
