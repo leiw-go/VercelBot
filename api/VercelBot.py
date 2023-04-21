c57ff3fcdd2702be3fe34927ac8323420b2d14df
leiw <i.wanglei@foxmail.com>
2023-04-21 16:29:36 +0800

    add dispatch

--- api/VercelBot.py
+++ api/VercelBot.py
@@ -1,21 +1,34 @@
 from flask import Flask, request
-import telegram
+from telegram import Bot, Update
 import os
+from telegram.ext import Dispatcher, MessageHandler, Filters
+from httpx import AsyncClient
 
 app = Flask(__name__)
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
+
 # 用你的bot token替换
-bot = telegram.Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN)
+dispatcher = Dispatcher(bot, None, workers=0)
+client = AsyncClient(limits={"max_connections": 100})
+
+
+def handle_message(update: Update, context):
+    # 在这里处理接收到的消息
+    chat_id = update.message.chat_id
+    text = update.message.text
+    bot.send_message(chat_id=chat_id, text=f"您发送了: {text}")
+
+
+dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
 
 
 @app.route('/webhook', methods=['POST'])
-async def webhook():
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    chat_id = update.message.chat.id
-    text = update.message.text
-    await bot.sendMessage(chat_id=chat_id, text=text)
-    return 'ok'
+def webhook():
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
+    return "OK"
 
 
 @app.route('/', methods=['GET'])
