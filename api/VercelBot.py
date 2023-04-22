5ae7f6ebf7519caaef41e0c472a93e98f4f79f6c
leiw <i.wanglei@foxmail.com>
2023-04-22 13:22:32 +0800

    back to 13.7

--- api/VercelBot.py
+++ api/VercelBot.py
@@ -2,7 +2,7 @@ from flask import Flask, request, g
 from telegram import Bot, Update
 import leancloud
 import os
-from telegram.ext import Dispatcher, MessageHandler, Filters
+from telegram.ext import Dispatcher, MessageHandler, Filters, CallbackContext, CommandHandler
 from httpx import AsyncClient
 
 app = Flask(__name__)
@@ -21,13 +21,21 @@ leancloud.init(APP_ID, APP_KEY)
 MyTable = leancloud.Object.extend('links')
 
 
-def handle_message(update: Update, context):
+async def command_all(update: Update, context: CallbackContext):
+    query = MyTable.query
+    results = query.find()
+    if results is None:
+        bot.sendMessage(chat_id=update.effective_chat.id, text='links are empty, add your collected link')
+    else:
+        links = [row.get('link') for row in results]
+        bot.send_message(chat_id=update.effective_chat.id, text='\n'.join(links))
+    return 'ok'
+
+def message_group_link(update: Update, context):
     # 在这里处理接收到的消息
     chat_id = update.message.chat_id
     text = update.message.text
-    if text == '/all':
-        handle_all(update)
-    elif text == '/great':
+    if text == '/great':
         handle_great(update)
     elif text.startswith('http') or text.startswith('https'):
         handle_add_link(update)
@@ -36,7 +44,8 @@ def handle_message(update: Update, context):
         bot.send_message(chat_id=chat_id, text=f"您发送了: {text}")
 
 
-dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
+dispatcher.add_handler(CommandHandler('/all', command_all))
+dispatcher.add_handler(MessageHandler(Filters.text, message_group_link))
 
 
 @app.route('/webhook', methods=['POST'])
